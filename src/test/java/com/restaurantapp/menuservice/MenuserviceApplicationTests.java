f292cbbd667cf1ce0b8d189612a1e98c4b55493e
Łukasz Wojewnik <l.wojewnik@wp.pl>
2025-07-29 12:27:31 +0200

    init

---
+++ src/test/java/com/restaurantapp/menuservice/MenuserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restaurantapp.menuservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MenuserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
